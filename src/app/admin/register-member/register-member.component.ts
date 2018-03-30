7d7742df682edfc8946d7c5b6a18db1b1763dff8
wjkim81 <onejean81@gmail.com>
2018-03-31 03:41:57 +0900

    Add reacitve form for registering member

--- src/app/admin/register-member/register-member.component.ts
+++ src/app/admin/register-member/register-member.component.ts
@@ -8,6 +8,8 @@ import { Location } from '@angular/common';
 import { SUBJECTS, TYPES, COUNTRIES } from '../../shared/member-options';
 import { Member } from '../../shared/member';
 
+import { PasswordValidation } from '../../services/password-validation';
+
 @Component({
   selector: 'app-register-member',
   templateUrl: './register-member.component.html',
@@ -25,17 +27,59 @@ export class RegisterMemberComponent implements OnInit {
 
   formErrors = {
     'id': '',
-    'comment': ''
+    'password': '',
+    'confirmPassword': '',
+    'firstname': '',
+    'lastname': '',
+    'type': '',
+    'subject': '',
+    'country': '',
+    'city': '',
+    'location1': '',
+    'phoneNum': '',
+    'mobileNum': ''
   };
 
    validationMessages = {
     'id': {
       'required': 'ID is required.',
-      'minlength': 'ID must be at least 6 characters long',
+      'minlength': 'ID must be at least 4 characters long.',
     },
-    'comment': {
-      'required': 'Comment is required.'
-    }
+    'password': {
+      'required': 'Password is required.',
+      'minlength': 'Password must be at least 6 characters long.',
+    },
+    'confirmPassword': {
+      'required': 'Confirm Password is required.',
+      'MatchPassword': 'Password should match.',
+    },
+    'firstname': {
+      'required': 'First name is required.',
+    },
+    'lastname': {
+      'required': 'Last name is required.',
+    },
+    'type': {
+      'required': 'Type is required.',
+    },
+    'subject': {
+      'required': 'Subject is required.',
+    },
+    'country': {
+      'required': 'Country is required.',
+    },
+    'city': {
+      'required': 'City is required.',
+    },
+    'location1': {
+      'required': 'Address is required.',
+    },
+    'phoneNum': {
+      'required': 'Phone number is required.',
+    },
+    'mobileNum': {
+      'required': 'Mobile number is required.',
+    },
   };
   
   constructor(
@@ -48,6 +92,7 @@ export class RegisterMemberComponent implements OnInit {
     this.subjects = SUBJECTS;
     this.types = TYPES;
     this.countries = COUNTRIES;
+    //console.log(this.subjects);
 
     //console.log(this.BaseURL);
     this.createForm();
@@ -55,21 +100,24 @@ export class RegisterMemberComponent implements OnInit {
 
   createForm() {
     this.memberForm = this.fb.group({
-      id: ['', [Validators.required, Validators.minLength(2) ]],
-      password: ['', [Validators.required, Validators.minLength(6) ]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(6) ]],
-      firstname: ['', [Validators.required, Validators.minLength(2) ]],
-      lastname: ['', [Validators.required, Validators.minLength(2) ]],
-      type: ['', Validators.required],
-      subject: ['', Validators.required],
-      country: ['', Validators.required],
+      id: ['', [Validators.required, Validators.minLength(4) ]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(25) ]],
+      confirmPassword: ['', [Validators.required]],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      type: [this.types[0], Validators.required],
+      subject: [this.subjects[0], Validators.required],
+      country: [this.countries[0], Validators.required],
       city: ['', Validators.required],
-      location1: '',
+      location1: ['', Validators.required],
       location2: '',
       postalCode: '',
       managerName: '',
       phoneNum: ['', Validators.required],
       mobileNum: ['', Validators.required]
+    },
+    {
+      validator: PasswordValidation.MatchPassword
     });
 
     this.memberForm.valueChanges
@@ -80,13 +128,16 @@ export class RegisterMemberComponent implements OnInit {
   onValueChanged(data?: any) {
     if (!this.memberForm) { return; }
     const form = this.memberForm;
+    //console.log('confirmPassword: '+ this.formErrors['confirmPassword']);
     
     for (const field in this.formErrors) {
       // clear previous error message (if any)
       this.formErrors[field] = '';
       const control = form.get(field);
       if (control && control.dirty && !control.valid) {
+        //console.log(`${field} - dirty: ${control.dirty}, valid: ${control.valid}`);
         const messages = this.validationMessages[field];
+        //console.log(control.errors);
         for (const key in control.errors) {
           this.formErrors[field] += messages[key] + ' ';
         }
@@ -97,5 +148,23 @@ export class RegisterMemberComponent implements OnInit {
   submitMember() {
     this.newMember = this.memberForm.value;
     console.log(this.newMember);
+
+    this.memberForm.reset({
+      id: '',
+      password: '',
+      confirmPassword: '',
+      firstname: '',
+      lastname: '',
+      type: this.types[0],
+      subject: this.subjects[0],
+      country: this.countries[0],
+      city: '',
+      location1: '',
+      location2: '',
+      postalCode: '',
+      managerName: '',
+      phoneNum: '',
+      mobileNum: ''
+    });
   }
 }
